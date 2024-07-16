07d064d8f68a26ac51dc96d0d7139bfafa9679f5
douglascdev <douglasc.dev@gmail.com>
2024-07-16 14:19:04 -0300

    Initialize lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,17 @@
+/*
+Takes source code as input and output the tokens that represent the source code
+*/
+
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input(points to current char)
+	readPosition int  // current reading position in input(after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
