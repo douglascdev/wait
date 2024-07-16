704c7005aac24b07acc58431ed5f0a95bf02c07f
douglascdev <douglasc.dev@gmail.com>
2024-07-16 15:29:16 -0300

    Skip whitespace and other unnecessary characters

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -48,9 +48,17 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[startPos:l.position]
 }
 
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 // Look at current character under examination and return a token depending on which character(s) it is
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
